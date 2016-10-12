004661fb38d2b24e38320184b63137336fe79ca4
jh1214 <jh1214@NCDTL022JH1214.ITServices.sbc.com>
2016-10-11 20:20:46 -0400

    ACCUMULO-4413 adding INFO logs for random port assignment

--- server/base/src/main/java/org/apache/accumulo/server/rpc/TServerUtils.java
+++ server/base/src/main/java/org/apache/accumulo/server/rpc/TServerUtils.java
@@ -267,6 +267,8 @@ public class TServerUtils {
 
     if (address.getPort() == 0) {
       address = HostAndPort.fromParts(address.getHostText(), transport.getServerSocket().getLocalPort());
+      log.info("Blocking Server bound on {}", address);
+
     }
 
     return new ServerAddress(server, address);
@@ -379,6 +381,8 @@ public class TServerUtils {
 
     if (address.getPort() == 0) {
       address = HostAndPort.fromParts(address.getHostText(), transport.getServerSocket().getLocalPort());
+      log.info("SSL Thread Pool Server bound on {}", address);
+
     }
 
     ThreadPoolExecutor pool = createSelfResizingThreadPool(serverName, numThreads, numSimpleTimerThreads, timeBetweenThreadChecks);
